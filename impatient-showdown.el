c29660b47f7d1764b12410dc8840f4fc70e45c99
jenchieh <jcs090218@gmail.com>
2020-08-31 16:59:49 +0800

    Identify first version.

--- impatient-showdown.el
+++ impatient-showdown.el
@@ -6,7 +6,7 @@
 ;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
 ;; Description: Preview markdown buffer live over HTTP.
 ;; Keyword: live preview markdown http server impatient
-;; Version: 0.0.1
+;; Version: 0.1.0
 ;; Package-Requires: ((emacs "24.3") (impatient-mode "1.1"))
 ;; URL: https://github.com/jcs-elpa/impatient-showdown
 
