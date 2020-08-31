20f598d524c0c52893f1b5ae74db5becb8539c08
jenchieh <jcs090218@gmail.com>
2020-08-31 16:12:39 +0800

    Add core.

---
+++ impatient-showdown.el
@@ -0,0 +1,38 @@
+;;; impatient-showdown.el --- Preview markdown buffer live over HTTP  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2020  Shen, Jen-Chieh
+;; Created date 2020-08-31 16:08:08
+
+;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
+;; Description: Preview markdown buffer live over HTTP.
+;; Keyword: live preview markdown http server impatient
+;; Version: 0.0.1
+;; Package-Requires: ((emacs "24.3"))
+;; URL: https://github.com/jcs090218/impatient-showdown
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; Preview markdown buffer live over HTTP.
+;;
+
+;;; Code:
+
+
+
+(provide 'impatient-showdown)
+;;; impatient-showdown.el ends here
